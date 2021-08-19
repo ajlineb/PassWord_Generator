47e081aa00d721f383e258631c891970589d2020
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2021-08-18 21:55:09 -0400

    created several new arrays, characterList now grabs each type of character type the user wants, Combine function created, creates two new arrays finalPassword and randomArray. Inside this function has a for loop for the length of the length variable. randomArray will grab a random variable Array from characterList and then finalPassWord will grab a random character from that chosen variable array and add it to itself. This will continue until the for loop finishes.

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,17 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = [];
-var upperCase = [];
-var specialCharacter = [];
-var number = [];
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialCharacter = ["!","#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","?","@","[","]","^","_","`","{","}","~"];
+var number = ["1","2","3","4","5","6","7","8","9","0"];
 
-
-
+// var randLower = [];
+// var randUpper = [];
+// var randSpecial = [];
+// var randNumber = [];
 
 function generatePassword() {
   alert("Lets Create a Password!");
   var isNum = true;
+  //var finalArray = [];
 
   //determines how many characters long while also determining if it is more than 8, less than 128, and if it is a number
   while (isNum === true) {
@@ -36,26 +39,39 @@ function generatePassword() {
   //Askes for the type of characters to be added
   function typeOfCharacters(){
     isLowerCase = confirm("Would you like lower case characters?");
+    characterList =[];
+
+    //checks if user wants lower case characters
     if (isLowerCase === true) {
       alert("success!");
+      characterList.push(lowerCase);
     }else {
       alert("no lower case!");
     };
+
+    //checks if user wants upper case characters
     isUpperCase = confirm("Would you like upper case characters?");
     if (isUpperCase === true) {
       alert("success!");
+      characterList.push(upperCase);
     }else {
       alert("no upper case!");
     };
+
+    //checks if user wants special characters
     isSpecialCharacter = confirm("Would you like special characters?");
     if (isSpecialCharacter === true) {
       alert("success!");
+      characterList.push(specialCharacter);
     }else {
       alert("no special characters!");
     };
+
+    //checks if user wants numbers
     isNumeric = confirm("Would you like numbers?");
     if (isNumeric === true) {
       alert("success!");
+      characterList.push(number);
     }else {
       alert("no numbers!");
     };
@@ -64,8 +80,29 @@ function generatePassword() {
     if (isLowerCase === false && isUpperCase === false && isSpecialCharacter === false && isNumeric === false) {
       alert("Need at least one catogory to be selected!");
       typeOfCharacters();
-    };
+    }else {
+      console.log(characterList);
+      combine();
+    }
   };
+
+  //combines the desired characters with the desired length using a for loop
+  function combine() {
+    var finalPassWord = [];
+    var randomArray = [];
+
+    for (let i = 0; i < length; i++) {
+      //picks a random variable array with the characters the user wanted
+      randomArray = characterList[Math.floor(Math.random()*characterList.length)];
+      
+      //puts in a random character from the random array into the final password
+      finalPassWord += randomArray[Math.floor(Math.random()*randomArray.length)];
+      
+    }
+    console.log(finalPassWord);
+    return finalPassWord;
+  }
+
 };
 
 
@@ -81,4 +118,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
+generateBtn.addEventListener("click", writePassword);
